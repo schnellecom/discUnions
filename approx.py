3baf8a1707c82beb1f2d3af1ea9f8f6559c88166
Lukas Schnelle <lukas.schnelle1@rwth-aachen.de>
2023-09-29 09:05:01 +0200

    remove squared error and change to absolute one, increase number of iterations

--- approx.py
+++ approx.py
@@ -5,7 +5,7 @@ import networkx as nx
 
 dim = 3
 eps = 0.01
-nIterations = 1
+nIterations = 100
 
 vertices = [ 1, 4, 24, 27, 32, 33, 34, 35, 36, 37 ]
 edges = [ [ 1, 35 ], [ 1, 34 ], [ 1, 36 ], [ 1, 4 ], [ 4, 36 ], [ 1, 32 ], [ 4, 32 ], [ 1, 33 ], [ 4, 37 ], [ 24, 32 ], [ 24, 33 ], [ 24, 37 ], [ 24, 27 ], [ 27, 37 ], [ 24, 35 ], [ 27, 35 ], [ 24, 34 ],[ 27, 36 ], [ 32, 33 ], [ 33, 34 ], [ 34, 35 ], [ 35, 36 ], [ 36, 37 ], [ 32, 37 ] ]
@@ -40,7 +40,7 @@ def oneError(arr, i):
     dist = (coordsa[0]-coordsb[0])**2 + (coordsa[1]-coordsb[1])**2 + (coordsa[2]-coordsb[2])**2
     dist = np.sqrt(dist)
     
-    return (dist-1)**2
+    return abs(dist-1)
 
 def error(arr):
     error = 0
@@ -57,8 +57,8 @@ iter = 1
 bestPos = []
 errorArr = []
 
-while currError > 0.01 and iter <= nIterations:
-    pos = nx.spring_layout(G, k=1, dim=3, threshold=1e-16, iterations=1000000)
+while currError > 0.001 and iter <= nIterations:
+    pos = nx.spring_layout(G, k=1, dim=3, threshold=1e-8, iterations=10000000)
     currError = error(errorArray(pos))
     if minError > currError:
         minError = currError
@@ -67,7 +67,7 @@ while currError > 0.01 and iter <= nIterations:
     iter += 1
 print("\nbest found error: ",minError)
 for i in range(len(edges)):
-    errorArr.append(np.sqrt(oneError(errorArray(bestPos), i)))
+    errorArr.append((oneError(errorArray(bestPos), i)))
 
 print("\n\n",niceArray(bestPos))
 print("\nerror for edges is: ", errorArr)
